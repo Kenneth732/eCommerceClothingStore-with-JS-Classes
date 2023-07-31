eb239fd39a7e5942b4db3678f75a872b6207e9a8
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-31 16:23:53 +0300

    selecting Element and setting _totalPriceElem.. private

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,11 @@
 class ClothingStore {
+  constructor() {
+    this.clothingList = document.getElementById('clothingList');
+    this.cartItems = document.getElementById('cartItems');
+    this._totalPriceElement = document.getElementById('totalPrice'); // Use a private property with an underscore
+    this.cartIcon = document.getElementById('cartIcon');
+
+  }
 
 }
 
